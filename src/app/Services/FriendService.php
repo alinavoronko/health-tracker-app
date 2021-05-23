03d54943bb5fd2761aa7b666d7932b2af4e0ab7c
Artjoms Travkovs <atravkovs@gmail.com>
2021-05-23 23:17:45 +0300

    Add trainers

--- src/app/Services/FriendService.php
+++ src/app/Services/FriendService.php
@@ -25,13 +25,15 @@ class FriendService
         return $this->mapperService->toModel($response->collect(), Friend::class);
     }
 
-    public function addFriend($userId, $friendId) {
+    public function addFriend($userId, $friendId)
+    {
         $response = Http::post($this->constructUrl($userId) . '?friendId=' . $friendId);
 
         return $this->mapperService->mapper(Friend::class, $response->json());
     }
 
-    public function removeFriend($userId, $friendId) {
+    public function removeFriend($userId, $friendId)
+    {
         $response = Http::delete($this->constructUrl($userId) . '?friendId=' . $friendId);
 
         return $response->successful();
@@ -46,15 +48,39 @@ class FriendService
         return $this->mapperService->toModel($response->collect(), FriendRequest::class);
     }
 
-    public function acceptFriendRequest($userId, $friendId) {
+    public function acceptFriendRequest($userId, $friendId)
+    {
         return $this->updateFriendRequest($userId, $friendId, 'ACCEPTED');
     }
 
-    public function declineFriendRequest($userId, $friendId) {
+    public function declineFriendRequest($userId, $friendId)
+    {
         return $this->updateFriendRequest($userId, $friendId, 'DECLINED');
     }
 
-    private function updateFriendRequest($userId, $friendId, $state) {
+    public function getTrainer($userId)
+    {
+        $url = $this->constructTrainerUrl($userId);
+
+        $trainer = Http::get($url);
+
+        if ($trainer->body() == 'null') return null;
+
+        return $this->mapperService->mapper(Friend::class, $trainer->json());
+    }
+
+    public function setTrainer($userId, $trainerId, $revoke = false)
+    {
+        $revokeRequest = $revoke ? 'true' : 'false';
+        $request = $this->constructRequestUrl($userId) . '?revoke=' . $revokeRequest . '&trainerId=' . $trainerId;
+
+        $response = Http::put($request);
+
+        return $this->mapperService->mapper(Friend::class, $response->json());
+    }
+
+    private function updateFriendRequest($userId, $friendId, $state)
+    {
         $requestUrl = $this->constructRequestUrl($userId) . '?friendId=' . $friendId . '&requestState=' . $state;
 
         $response = Http::put($requestUrl);
@@ -62,12 +88,18 @@ class FriendService
         return $this->mapperService->mapper(FriendRequest::class, $response->json());
     }
 
-    private function constructRequestUrl($userId) {
+    private function constructRequestUrl($userId)
+    {
         return $this->constructUrl($userId) . '/request';
     }
 
     private function constructUrl($userId)
     {
         return $this->url . 'user/' . $userId . "/friend";
+    }
+
+    private function constructTrainerUrl($userId)
+    {
+        return $this->url . 'user/' . $userId . '/trainer';
     }
 }
